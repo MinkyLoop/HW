8697a770f192d204876d272cd2a633c9fc86884f
MinkyLoop <gebeldanilalenovoa536@gmail.com>
2025-02-19 18:31:25 +0200

    правки

--- 2 курс/4 семестр/Дискретка/lab_2(5).py
+++ 2 курс/4 семестр/Дискретка/lab_2(5).py
@@ -1,20 +1,20 @@
 from functools import lru_cache
-
+@lru_cache(None)
 def count_shortest_paths(n, m):
-    if n == 0 or m == 0:
-        return 1
+
+    if n == 0 or m == 0: return 1
+
     return count_shortest_paths(n - 1, m) + count_shortest_paths(n, m - 1)
 
 @lru_cache(None)
 def count_paths_no_consecutive_vertical(n, m, last_vertical=False):
-    if n == 0 and m == 0:
-        return 1
+
+    if n == 0 and m == 0: return 1
     
     paths = 0
-    if n > 0:
-        paths += count_paths_no_consecutive_vertical(n - 1, m, False)
-    if m > 0 and not last_vertical:
-        paths += count_paths_no_consecutive_vertical(n, m - 1, True)
+
+    if n > 0: paths += count_paths_no_consecutive_vertical(n - 1, m, False)
+    if m > 0 and not last_vertical: paths += count_paths_no_consecutive_vertical(n, m - 1, True)
     
     return paths
 
